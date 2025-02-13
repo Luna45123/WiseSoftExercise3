16ede283c8bcf6f9056d3a744ba23084650ff3f1
Sunny4826 <118628971+Luna45123@users.noreply.github.com>
2025-02-13 11:10:17 +0700

    a

---
+++ src/main/java/com/wisesoft/seminar/service/ExtractDurationService.java
@@ -0,0 +1,18 @@
+package com.wisesoft.seminar.service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExtractDurationService {
+    public int extractDuration(String title) {
+        Pattern DURATION_PATTERN = Pattern.compile("\\s(\\d+)min$");
+        Matcher matcher = DURATION_PATTERN.matcher(title);
+        if (matcher.find()) {
+            return Integer.parseInt(matcher.group(1));
+        }
+        throw new IllegalArgumentException("Invalid format: " + title); // Handle cases where duration is missing
+    }
+}
